132045d766d384c8b215d21874fff1cd02361cc6
Meshak <meshcrescent@gmail.com>
2025-10-27 08:13:35 -0400

    Added the display and hiding of the hambuger button

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
 const bgColor = document.querySelector("body")
 bgColor.style.backgroundColor = "plum"
+
+const displayBtn = document.querySelector(".hambuger-btn")
+const hideBtn = document.querySelector(".cancel-btn")
+const mainContainer = document.querySelector(".main-container")
+
+displayBtn.addEventListener("click", ()=> {
+    mainContainer.style.display = "block"
+    displayBtn.style.display = "none"
+})
+
+hideBtn.addEventListener("click", ()=> {
+    mainContainer.style.display = "none"
+    displayBtn.style.display = "block"
+})
